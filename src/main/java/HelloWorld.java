5303cad654d82c444c79a2d1f58d45af0ba5c13a
ma.qiyue <q_yma>
2018-05-07 14:14:13 +0800

    edit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("112312323");
+        System.out.println(12123);
     }
 }
